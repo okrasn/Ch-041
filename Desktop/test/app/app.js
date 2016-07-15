cd02900de12d1b5a881cf30a0c1c4a725e673608
Kasjs <gemyni85@gmail.com>
2016-07-15 12:12:47 +0300

    Complete table

--- Desktop/test/app/app.js
+++ Desktop/test/app/app.js
@@ -1,49 +1,49 @@
-angular.module('schedule',['tuls','ui.router'])
+angular.module('schedule',['datepicker' ,'accordion','ui.router'])
     .config(function($stateProvider,$urlRouterProvider){
         $urlRouterProvider.otherwise('/groups');
         $stateProvider
-            .state('groups',{
-                url : '/groups',
-                templateUrl : 'partials/groups.html',
-                controller : 'AccordionCtrl'
-        	})
-            .state('groups.group-calendar-1',{
-                url : '/groups',
-                templateUrl : 'partials/group-calendar-1.html',
+            .state('groups', {
+                url: '/groups',
+                templateUrl:'partials/groups.html',
                 controller : 'AccordionCtrl'
             })
-            .state('groups.group-calendar-1',{
-                url : '/groups',
-                templateUrl : 'partials/group-calendar-1.html',
+            .state('groups.groupsCalendar', {
+                url: '/groupsCalendar/:groupsCalendarID/',
+                templateUrl: 'partials/groups.calendar.html',
                 controller : 'AccordionCtrl'
             })
-            .state('groups.group-calendar-1',{
-                url : '/groups',
-                templateUrl : 'partials/group-calendar-1.html',
+            .state('groups.groupsCalendar.table', {
+                url: '/table/:tableID',
+                templateUrl: 'partials/table.html',
                 controller : 'AccordionCtrl'
-            })
-
-        	
-    })
-    .controller('TableCtrl',['$scope',function($scope){
-    	
-    }])
-    .factory('transfer', function($http){
-        var data = [];
-        $http.get('data/custom.json').then(function(response){
-            console.log(response.data.groups);
-            data.push(response.data.groups);
         })
+    })
+    .controller('TableCtrl',['$scope','$http','transfer',function($scope,$http,transfer){
+        $scope.data = [];
+        $scope.groups = transfer.getData();
+        $http.get('data/custom.json').then(function(response){
+            console.log(response.data);
+            var length = response.data.groups.length;
+            for(var i = 0;i < length; i++){
+                $scope.data.push(response.data.groups[i]);
+            }
+        },function(response){
+            alert('Error no response');
+        })
+    }])
+    .factory('transfer',[function(){
+        var transferData = [];
         function setData(data){
-            date.push(data);
+            transferData.push(data);
         }
-
-        function getData(data){
-            return data;
+        function getData(){
+            return transferData;
         }
-        console.log(data);
-        return {
+        return{
             setData : setData,
             getData : getData
         }
-    });
+    }]);
+
+
+
---
+++ Desktop/test/app/js/accordion.js
@@ -0,0 +1,19 @@
+angular.module('accordion', ['ngAnimate', 'ui.bootstrap'])
+    .controller('AccordionCtrl', function ($scope,$http,transfer,$stateParams) {
+        $scope.oneAtATime = true;
+        $scope.status = {
+            isCustomHeaderOpen: false,
+            isFirstOpen: true,
+            isFirstDisabled: false
+        };
+        $scope.groupsData = [];
+        $scope.dayData = [];
+
+        $http.get('data/custom.json').then(function(response){
+            var length = response.data.groups.length;
+            for(var i = 0; i < length;i++){
+                $scope.groupsData.push(response.data.groups[i]);
+            }
+        });
+
+    })
