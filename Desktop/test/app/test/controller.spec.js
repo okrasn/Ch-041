acdd8e91e4b64d1b01d75b3cc97137f78f2b1afe
Kasjs <gemyni85@gmail.com>
2016-07-19 12:51:33 +0300

    fixed conflict

--- Desktop/test/app/test/controller.spec.js
+++ Desktop/test/app/test/controller.spec.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
 
-describe('AccordionCtrl', function() {
-    beforeEach(module('schedule'));
-    it('should contain `test` number 2', inject(function($controller){
-        var scope = {};
-        var ctrl = $controller('AccordionCtrl',{$scope : scope});
-        expect(scope.test).toBe(2);
-    }));
-=======
 describe('AccordionCtrl', function() {
 	beforeEach(module('accordion'))
 it('should contain `groupsData` with 3 groups', inject(function($controller){
@@ -15,6 +6,4 @@ it('should contain `groupsData` with 3 groups', inject(function($controller){
 	var ctrl = $controller('AccordionCtrl',{$scope : scope})
 	expect(scope.groupsData.length.toBe(3));
 }));
->>>>>>> 989a5a18ee33fbdab85c3b1119658e72c0ee7da1
-
 });
