9ff9ae82c4de4cd0412d3ac92224aa7fa023a957
Kasjs <gemyni85@gmail.com>
2016-07-20 12:36:20 +0300

    select does not work yet

--- app/js/accordion.js
+++ app/js/accordion.js
@@ -7,19 +7,34 @@ angular.module('accordion', ['ngAnimate', 'ui.bootstrap'])
             isFirstDisabled: false
         };
         //$scope.counter = 0;
-         $scope.passingData = function(){
+        $scope.passingData = function(){
             count_service.setData($scope.search_subject);
             $scope.subject = count_service.getData();
             var counter = 0;
-            for(var i = 0; i < $scope.element.days.length;i++){
-               for(var j = 0; j < $scope.element.days[i].subject.length;j++){
-                    if($scope.subject.toLowerCase() === $scope.element.days[i].subject[j].subject){
-                        counter++;
-                    }
-                }
-            }
+             for(var i = 0; i < $scope.element.days.length;i++){
+                 for(var j = 0; j < $scope.element.days[i].subject.length;j++){
+                     if($scope.subject.toLowerCase() === $scope.element.days[i].subject[j].subject) {
+                         counter++;
+                     }
+                 }
+             }
             $scope.counter = counter;
             $scope.search_subject = '';
+        };
+        $scope.setSubjects = function(){
+            for(var i = 0; i < $scope.element.days.length;i++){
+                for(var j = 0; j < $scope.element.days[i].subject.length;j++){
+                    $scope.options = [];
+                    angular.forEach($scope.options ,function(value){
+                        if(value == '' ){
+                            $scope.options.push($scope.element.days[i].subject[j].subject)
+                        }else if(value != $scope.element.days[i].subject[j].subject){
+                            $scope.options.push($scope.element.days[i].subject[j].subject)
+                        }
+                    });
+                }
+            }
+            console.log($scope.options);
         };
         $scope.test = 2;
         transfer.callback().then(function(response){
--- app/schedule.spec.js
+++ app/schedule.spec.js
@@ -1,7 +1,20 @@
+/*
 describe('Schedule App', function() {
     it('should have a title', function() {
         browser.get('http://192.168.1.100:3000');
 
         expect(browser.getTitle()).toEqual('Schedule App');
+    });
+});
+*/
+
+
+
+describe('Schedule App', function() {
+    it('should click on first group', function() {
+        browser.get('http://192.168.1.100:3000');
+        element(by.binding('QA developers')).click();
+        element(by.model('search_subject')).sendKeys('qa');
+
     });
 });
