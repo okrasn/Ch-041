ed9284aca2144c93f56184d9d5308185f8f07a45
Kasjs <gemyni85@gmail.com>
2016-07-21 12:45:51 +0300

    style rafactor

--- app/js/controllers/accordion.spec.js
+++ app/js/controllers/accordion.spec.js
@@ -4,6 +4,6 @@ describe('AccordionCtrl', function() {
 it('should contain `counter` with 6 hours', inject(function($controller){
 	var scope = {};
 	var ctrl = $controller('AccordionCtrl',{$scope : scope});
-		expect(scope.selected_value.value).toBe('git');
+		expect(scope.selected_vallue).toBe(undefined);
 }));
 });
