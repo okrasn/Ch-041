68435980d300589041f9c3d7cc0428c40eb83269
Kasjs <gemyni85@gmail.com>
2016-07-22 12:53:20 +0300

    karma test updating

--- app/js/controllers/accordion.spec.js
+++ app/js/controllers/accordion.spec.js
@@ -1,10 +1,12 @@
-/*
+
+
+
 describe('AccordionCtrl', function() {
-	beforeEach(angular.mock.module('accordion'));
+
+	beforeEach(module('schedule'));
 it('should contain `counter` with 6 hours', inject(function($controller){
 	var scope = {};
 	var ctrl = $controller('AccordionCtrl',{$scope : scope});
 		expect(scope.selected_vallue).toBe(undefined);
 }));
 });
-		*/
--- app/js/services/count_srvice.spec.js
+++ app/js/services/count_srvice.spec.js
@@ -12,4 +12,75 @@ var count_service;
             expect(count_service.counter()).toBeDefined();
         });
     });
+    describe('.getData()', function() {
+        it('should exist', function() {
+            //expect(count_service.setData('git'));
+            //expect(count_service.getData()).toEqual('git');
+
+            //expect(count_service.getArrayObject()).toEqual(['git','git','git','git','git']);
+            //expect(count_service.counter()).toEqual(0);
+
+        });
+    });
+    describe('count service', function() {
+        it('should count the number of same subject and return number', function() {
+            var enteredSubject = 'git',counter = 0;
+            var arrayObjects = {
+
+                subject :[
+                    {
+                        time : "09:00-09:55",
+                        subject : "git"
+                    },{
+                        time : "10:00-10:55",
+                        subject : "git"
+
+                    },{
+                        time : "11:00-11:55",
+                        subject : "git"
+
+                    },{
+                        time : "12:00-12:55",
+                        subject : "git"
+
+                    },{
+                        time : "13:00-13:55",
+                        subject : "git"
+
+                    },{
+                        time : "14:00-14:55",
+                        subject : "git"
+                    }
+                ]
+
+
+
+            };
+
+                for(var i = 0; i < 6;i++){
+                    for(var j = 0; j < arrayObjects.subject.length;j++){
+                        if(enteredSubject === arrayObjects.subject[j].subject) {
+                            counter++;
+                        }
+                    }
+                }
+
+
+
+            expect(count_service.setArrayObjects(arrayObjects));
+            expect(count_service.getArrayObject());
+            expect(count_service.counter()).toEqual(6);
+
+
+
+
+
+
+
+
+
+
+
+        });
+    });
 });
