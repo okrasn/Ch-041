84b376248e4c9143b3285efd410a6db10ddfbb1f
Valik <valik.stets@gmail.com>
2016-07-28 20:33:35 +0300

    Loginand register options

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,44 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
 var useref = require('gulp-useref');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var watch = require('gulp-watch');
+var sourcemaps = require('gulp-sourcemaps');
+var ngHtml2Js = require("gulp-ng-html2js");
+var ngAnnotate = require('gulp-ng-annotate');
 
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
     return gulp.src('client/scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('client/css'));
 });
 
-gulp.task('useref',function(){
+gulp.task('scripts', function () {
+    gulp.src(['./client/js/**/*.js', '!./client/js/**/*.test.js', '!./client/js/app.min.js'])
+        .pipe(concat('app.min.js'))
+        .pipe(uglify()).on('error', function (e) {
+            console.log(e);
+        })
+        .pipe(gulp.dest('./client/js/'))
+});
+
+gulp.task('useref', function () {
     return gulp.src('client/*.html')
         .pipe(useref())
         .pipe(gulp.dest('min'))
 });
 
-gulp.task('browserSync',function(){
-    browserSync.init({
-        server :{
-            baseDir : 'client'
-        }
-    })
-
-    gulp.watch('./client/scss/*.scss',['sass']);
-    gulp.watch('./client/**/*.js').on('change', browserSync.reload);
-    gulp.watch('./client/**/*.css').on('change', browserSync.reload);
-    gulp.watch('./client/**/*.html').on('change', browserSync.reload);
-    gulp.watch('./client/**/*.json').on('change', browserSync.reload);
+gulp.task('main', function () {
+    gulp.watch('./client/scss/*.scss', ['sass']);
+    gulp.watch('./client/**/*.js').on('change');
+    gulp.watch('./client/**/*.css').on('change');
+    gulp.watch('./client/**/*.html').on('change');
+    gulp.watch('./client/**/*.json').on('change');
+    gulp.watch(['./client/js/**/*.js', '!./client/js/**/*.test.js', '!./client/js/app.min.js'], function () {
+        gulp.start('scripts');
+    });
 });
 
-gulp.task('default', ['sass','browserSync']);
-
-
+gulp.task('default', ['scripts']);
---
+++ server/routes/index.js
@@ -0,0 +1,84 @@
+var mongoose = require('mongoose'),
+    express = require('express'),
+    router = express.Router(),
+    passport = require('passport'),
+    jwt = require('express-jwt'),
+    Feed = mongoose.model('Feed'),
+    User = mongoose.model('User');
+
+var auth = jwt({
+    secret: 'MY_SECRET',
+    userProperty: 'payload'
+});
+
+var authCtrl = require('../controllers/authentication');
+
+router.post('/register', authCtrl.register);
+
+router.post('/login', authCtrl.login);
+
+router.param('user', function (req, res, next, id) {
+    var query = User.findById(id);
+
+    query.exec(function (err, user) {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return next(new Error('can\'t find user'));
+        }
+        req.user = user;
+        return next();
+    });
+});
+
+// get user and his feeds
+router.get('/users/:user', auth, function (req, res, next) {
+    req.user.populate('feeds', function (err, user) {
+        if (err) {
+            return next(err);
+        }
+        console.log("Sending user");
+        res.json(user);
+    });
+});
+
+// add new feed
+router.post('/users/:user/addFeed', auth, function (req, res, next) {
+    var feed = new Feed(req.body);
+    feed.user = req.user;
+
+    feed.save(function (err, feed) {
+        if (err) {
+            return next(err);
+        }
+
+        req.user.feeds.push(feed);
+        req.user.save(function (err, user) {
+            if (err) {
+                return next(err);
+            }
+            res.json(feed);
+        });
+    });
+});
+
+router.delete('/users/:user/deleteFeed/:id', function (req, res, next) {
+    return Feed.findById(req.params.id, function (err, feed) {
+        if(!feed) {
+            res.statusCode = 404;
+            return res.send({ error: 'Feed not found' });
+        }
+        return feed.remove(function (err) {
+            if (!err) {
+                return res.send({ status: 'OK' });
+            } else {
+                res.statusCode = 500;
+                log.error('Internal error(%d): %s',res.statusCode,err.message);
+                return res.send({ error: 'Server error' });
+            }
+        });
+    });
+});
+
+module.exports = router;
