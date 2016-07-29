257131e5c9e23f2f724e31693b2987c404076b7b
Anatole <gemyni85@gmail.com>
2016-07-29 13:04:46 +0300

    add
    add

--- client/js/services/add_feed_service.js
+++ client/js/services/add_feed_service.js
@@ -1,15 +1,37 @@
 angular.module('add_service',[])
-    .factory('add_feed_service',[function(){
+    .factory('add_feed_service',['$http',function($http){
         var transferArray = [];
-        function setArray(array){
-            transferArray = array;
+        var urlTemp = '';
+        var feeds = [];
+        function setUrl(url){
+            urlTemp = url;
+        }
+        function getUrl(){
+            return urlTemp;
+        }
+
+        function setArray(obj){
+            transferArray = obj;
         }
         function getArray(){
             return transferArray;
         }
-        console.log(transferArray);
+        function getFeeds(){
+            return $http.jsonp("https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=" + urlTemp +
+                "&callback=JSON_CALLBACK").then(function(response){
+                return response.data.responseData.feed.entries[0];
+            },function(response){
+                console.log('Error');
+            });
+        }
+
+
+       // console.log(urlTemp);
         return{
             setArray : setArray,
-            getArray : getArray
+            getArray : getArray,
+            setUrl : setUrl,
+            getUrl : getUrl,
+            getFeeds : getFeeds
         }
     }]);
