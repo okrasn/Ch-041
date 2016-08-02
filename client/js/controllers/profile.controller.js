c996ba99af7ffa72dd345aaecb451c75c290fd9c
Olga Chrevko <marshak.oggy@gmail.com>
2016-08-02 15:40:52 +0300

    add styles to profile

--- client/js/controllers/profile.controller.js
+++ client/js/controllers/profile.controller.js
@@ -2,4 +2,5 @@
  * Created by olga on 02.08.16.
  */
 angular.module('rssreader').controller('ProfileController', ['$scope', '$state', 'authService', '$window', function ($scope, $state, authService, $window) {
+  $scope.user = {};
 }]);
