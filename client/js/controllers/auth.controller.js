a013a3d810feda25c749eae203fef889bf3baf24
Valik <valik.stets@gmail.com>
2016-08-02 16:06:05 +0300

    auth

--- client/js/controllers/auth.controller.js
+++ client/js/controllers/auth.controller.js
@@ -24,11 +24,75 @@ angular.module('rssreader').controller('AuthController', ['$scope', '$state', 'a
                 $window.onbeforeunload = $scope.onExit;
             }
             else
-            console.log("Checked");
+                console.log("Checked");
 
             $state.go('dashboard.th-large', {
                 id: authService.userID()
             });
         });
+    }
+
+
+    $scope.validationLoginOptions = {
+        rules: {
+            mail: {
+                required: true,
+                email: true
+            },
+            pwd: {
+                required: true
+            }
+        },
+        messages: {
+            mail: {
+                required: "This field is required ",
+                email: ""
+            },
+
+            pwd: {
+                required: "This field is required "
+            }
+        }
     };
+
+    $scope.validationRegistrOptions = {
+        rules: {
+            mail: {
+                required: true,
+                email: true,
+                minlength: 9,
+                maxlength: 20,
+            },
+            pwd: {
+                required: true,
+                minlength: 6,
+                maxlength: 20,
+            },
+            reppwd: {
+                required: true,
+                minlength: 6,
+                maxlength: 20,
+            }
+        },
+        messages: {
+            mail: {
+                required: "This field is required ",
+                email: "Please, use example: jacksparrow@gmail.com ",
+                minlength: "Please, enter at least 9 characters ",
+                maxlength: "Please, enter no more than 20 characters. "
+            },
+
+            pwd: {
+                required: "This field is required ",
+                minlength: "Please, enter at least 6 characters ",
+                maxlength: "Please, enter no more than 20 characters. "
+            },
+
+            reppwd: {
+                required: "This field is required ",
+                minlength: "Please, enter at least 6 characters ",
+                maxlength: "Please, enter no more than 20 characters. "
+            }
+        }
+    }
 }]);
