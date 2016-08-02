549da9db846e38f6f7450193d118ccb5cd963c50
Valik <valik.stets@gmail.com>
2016-08-02 16:17:39 +0300

    Merge 3

--- client/js/controllers/auth.controller.js
+++ client/js/controllers/auth.controller.js
@@ -1,37 +1,37 @@
 angular.module('rssreader').controller('AuthController', ['$scope', '$state', 'authService', '$window', function ($scope, $state, authService, $window) {
     $scope.user = {};
     $scope.session;
-    $scope.error = '';
 
-    $scope.register = function () {
-        authService.register($scope.user).error(function (error) {
-            $scope.error = error;
-        }).then(function () {
-            $state.go('home');
-        });
+    $scope.register = function (form) {
+        if (form.validate()) {
+            authService.register($scope.user).error(function (error) {
+                $scope.error = error;
+            }).then(function () {
+                $state.go('dashboard.th-large');
+            });
+        }
     };
 
-    $scope.logIn = function () {
-        $scope.error = '';
-        authService.logIn($scope.user, $scope.session).error(function (error) {
-            $scope.error = error;
-        }).then(function () {
-            if (!$scope.session) {
-                console.log("Not checked");
-                $scope.onExit = function () {
-                    auth.logOut();
-                };
-                $window.onbeforeunload = $scope.onExit;
-            }
-            else
-                console.log("Checked");
+    $scope.logIn = function (form) {
+        if (form.validate()) {
+            authService.logIn($scope.user, $scope.session).error(function (error) {
+                $scope.error = error;
+            }).then(function () {
+                if (!$scope.session) {
+                    console.log("Not checked");
+                    $scope.onExit = function () {
+                        auth.logOut();
+                    };
+                    $window.onbeforeunload = $scope.onExit;
+                } else
+                    console.log("Checked");
 
-            $state.go('dashboard.th-large', {
-                id: authService.userID()
+                $state.go('dashboard.th-large', {
+                    id: authService.userID()
+                });
             });
-        });
-    }
-
+        }
+    };
 
     $scope.validationLoginOptions = {
         rules: {
