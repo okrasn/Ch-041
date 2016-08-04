f5fbcd617f0d58d6e9908fc20de22d6dc6269546
Olga Chrevko <marshak.oggy@gmail.com>
2016-08-04 11:52:26 +0300

    add img upload

--- client/js/controllers/profile.controller.js
+++ client/js/controllers/profile.controller.js
@@ -1,6 +1,18 @@
 /**
  * Created by olga on 02.08.16.
  */
-angular.module('rssreader').controller('ProfileController', ['$scope', '$state', function ($scope) {
-   
+angular.module('rssreader').controller('ProfileController', ['$scope', function ($scope) {
+   $scope.stepsModel = [];
+  
+  $scope.imageUpload = function (el) {
+    var imgreader = new FileReader();
+    imgreader.onload = $scope.imageIsLoaded;
+    imgreader.readAsDataURL(el.files[0]);
+  };
+  
+  $scope.imageIsLoaded = function (e) {
+    $scope.$apply(function () {
+      $scope.stepsModel.push(e.target.result);
+    })
+  }
 }]);
--- client/js/jqscripts/sidebar.js
+++ client/js/jqscripts/sidebar.js
@@ -1,19 +1,3 @@
-// function toggleSidebar() {
-//   $(".button").toggleClass("active");
-//   $("main").toggleClass("move-to-left");
-//   $(".sidebar-item").toggleClass("active");
-// }
-
-// $(".button").on("click tap", function() {
-//   toggleSidebar();
-// });
-
-// $(document).keyup(function(e) {
-//   if (e.keyCode === 27) {
-//     toggleSidebar();
-//   }
-// });
-
 
 (function () {
     $("#app-wrapper").on("click", "#menu-toggle", function (e) {
@@ -25,7 +9,4 @@
         $("#glyphToggle").toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
     });
     
-//    $("#sidebar").on("click", ".feeds", function () {
-//        $(".glyphFeeds").toggleClass('glyphicon-chevron-right glyphicon-chevron-down');
-//    });
 })();
