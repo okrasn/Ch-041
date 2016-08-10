18e545dfd5a16a741e6e9af8856e3437e3771197
Valik <valik.stets@gmail.com>
2016-08-10 16:28:44 +0300

    Clean-up master

--- client/js/controllers/index.controller.js
+++
@@ -1,4 +0,0 @@
-angular.module('rssreader').controller('IndexController', ['$scope', '$state', 'authService', '$window', 'themeService', function ($scope, $state, authService, $window, themeService) {
-    $scope.layout = themeService.getTheme;
-    $scope.text = "some text";
-}]);
