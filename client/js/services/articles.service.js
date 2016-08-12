9e0d121b7daa15d15c2774ce209dfb5dc07b66e6
Valik <valik.stets@gmail.com>
2016-08-12 16:22:31 +0300

    Fetching all images from all articles

--- client/js/services/articles.service.js
+++ client/js/services/articles.service.js
@@ -4,36 +4,68 @@ angular.module('rssreader').service('articlesService', ['$http', '$q', 'authServ
         isFavourites: false
     }
     var articles_num = 50;
+    var getImage = function (item, format) {
+        var source = "";
+        if (format === "RSS") {
+            if (!item.getElementsByTagName('enclosure').length) {
+                try {
+                    source = $(item).find('media\\:content, content')[0].getAttribute('url');
+                } catch (err) {
+                    source = "";
+                }
+                if (source == "" || source == undefined) {
+                    try {
+                        var content = document.createElement('div');
+                        content.innerHTML = item.getElementsByTagName('description')[0].textContent;
+                        source = $(content).find('img')[0].src;
+                    } catch (err) {
+                        source = "";
+                    }
+                }
+            } else {
+                source = item.getElementsByTagName('enclosure')[0].getAttribute('url');
+            }
+        } else if (format === "ATOM") {
+            if (!item.getElementsByTagName('enclosure').length) {
+                try {
+                    var content = document.createElement('div');
+                    content.innerHTML = item.getElementsByTagName('content')[0].textContent;
+                    source = $(content).find('img')[0].src;
+                } catch (err) {
+                    source = "";
+                }
+            }
+        }
+        return source;
+    }
+    var getContent = function (item, format) {
+        var content = "";
+        if (format === "RSS") {
+            try {
+                content = document.createElement('div');
+                content.innerHTML = item.getElementsByTagName('description')[0].textContent;
+                content = $(content).text();
+            } catch (err) {
 
+            }
+        } else if (format === "ATOM") {
+            content = $(item.getElementsByTagName('content')[0].childNodes[0].data).text();
+        }
+        return content;
+    }
     var fetchArticles = function (feed) {
         return $http.jsonp("https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=" + articles_num + "&q=" + encodeURIComponent(feed.rsslink) + "&method=JSONP&callback=JSON_CALLBACK&output=xml").then(function (response) {
-                var parser = new DOMParser();
-                var xmlDoc = parser.parseFromString(response.data.responseData.xmlString, "text/xml");
-                var items = [];
+                var parser = new DOMParser(),
+                    xmlDoc = parser.parseFromString(response.data.responseData.xmlString, "text/xml"),
+                    items = [];
                 if (feed.format === "RSS") {
                     items = xmlDoc.getElementsByTagName('item');
                     for (var i = 0; i < items.length; i++) {
                         var articleObj = {};
                         articleObj.title = items[i].getElementsByTagName('title')[0].innerHTML;
                         articleObj.link = items[i].getElementsByTagName('link')[0].textContent;
-                        // -- Image ---
-                        if (!items[i].getElementsByTagName('enclosure').length) {
-                            var foundImg = {
-                                src: ""
-                            }
-                            try {
-                                foundImg.src = $(items[i].getElementsByTagName("description")[0].textContent).find('img')[0].src;
-                            } catch (err) {
-                                foundImg.src = ""
-                            }
-                            articleObj.img = foundImg.src;
-                        } else {
-                            articleObj.img = items[i].getElementsByTagName('enclosure')[0].getAttribute('url');
-                        }
-                        // ---------
-                        var content = document.createElement('div');
-                        content.innerHTML = items[i].getElementsByTagName('description')[0].textContent;
-                        articleObj.content = $(content).text();
+                        articleObj.img = getImage(items[i], feed.format);
+                        articleObj.content = getContent(items[i], feed.format) || articleObj.title;
                         articleObj.date = items[i].getElementsByTagName('pubDate')[0].textContent;
                         obj.articles.push(articleObj);
                     }
@@ -41,20 +73,14 @@ angular.module('rssreader').service('articlesService', ['$http', '$q', 'authServ
                     items = xmlDoc.getElementsByTagName('entry');
                     for (var i = 0; i < items.length; i++) {
                         var articleObj = {};
-                        var img = $.parseHTML(items[i].getElementsByTagName('content')[0].childNodes[0].data)[0].src;
-                        if (img === undefined) {
-                            img = "";
-                        }
-                        articleObj.img = img;
                         articleObj.title = items[i].getElementsByTagName('title')[0].textContent;
                         articleObj.link = items[i].getElementsByTagName('id')[0].textContent;
-                        articleObj.content =
-                            $(items[i].getElementsByTagName('content')[0].childNodes[0].data).text();
+                        articleObj.img = getImage(items[i], feed.format);
+                        articleObj.content = getContent(items[i], feed.format) || articleObj.title;
                         articleObj.date = items[i].getElementsByTagName('published')[0].textContent;
                         obj.articles.push(articleObj);
                     }
                 }
-
             },
             function (err) {
                 console.log(err);
@@ -64,10 +90,6 @@ angular.module('rssreader').service('articlesService', ['$http', '$q', 'authServ
         return obj.isFavourites;
     }
     obj.getAllArticles = function () {
-        console.log("all");
-        //        return $timeout(function(){
-        //            console.log("Resolving");
-        //        }, 2000);
         obj.isFavourites = false;
         obj.articles.length = 0;
         dashboardService.setTitle("All");
@@ -107,19 +129,15 @@ angular.module('rssreader').service('articlesService', ['$http', '$q', 'authServ
     obj.getFavourites = function () {
         obj.isFavourites = true;
         obj.articles.length = 0;
-
         dashboardService.setTitle("Favourites");
         dashboardService.resetFeedId();
-
         console.log(feedsService.favourites);
         angular.copy(feedsService.favourites, obj.articles);
     }
     obj.getFavArticle = function (article) {
         obj.isFavourites = true;
-
         dashboardService.setTitle("Favourites");
         dashboardService.resetFeedId();
-
         obj.articles.length = 0;
         obj.articles.push(article);
     }
@@ -131,8 +149,6 @@ angular.module('rssreader').service('articlesService', ['$http', '$q', 'authServ
         });
     }
     obj.removeFavourite = function (article) {
-        //        console.log("Removing:");
-        //        console.log("ArticleId:" + article._id);
         return $http.delete('/users/' + authService.userID() + '/deleteFavFeed/' + article._id, {
             headers: {
                 Authorization: 'Bearer ' + authService.getToken()
@@ -140,4 +156,4 @@ angular.module('rssreader').service('articlesService', ['$http', '$q', 'authServ
         });
     }
     return obj;
-            }]);
+}]);
