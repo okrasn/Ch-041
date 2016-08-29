6048370e49cef7e47d55b89a3f3d07d1440cc09f
Oleg Odoviichuk <eRR0R4ik@yandex.ru>
2016-08-29 20:45:39 +0300

    fix v1

--- client/js/services/profile.service.js
+++ client/js/services/profile.service.js
@@ -8,22 +8,22 @@
                 this.image = "";
                 this.setImagePath = function() {
 
-                     accountInfo.getProfile().then(function(response) {
-                    if ($auth.isAuthenticated()) {
-                        var lenght = response.data.user.length;
-                        for (var i = 0; i < lenght; i++) {
-                            if (response.data.user[i].email === $auth.getPayload().email) {
-                                obj.image = response.data.user[i].avatar;
+                    accountInfo.getProfile().then(function(response) {
+                        if ($auth.isAuthenticated()) {
+                            var lenght = response.data.user.length;
+                            for (var i = 0; i < lenght; i++) {
+                                if (response.data.user[i].email === $auth.getPayload().email) {
+                                    obj.image = response.data.user[i].avatar;
+                                }
                             }
                         }
-                    }
-                });
+                    });
                 }
 
                 this.getImage = function() {
                     return this.image;
                 }
-               this.setImagePath();
+                this.setImagePath();
             }
         ]);
 })();
