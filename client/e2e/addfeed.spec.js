3e6e0a3d9bd7d3cd9f80680dd357f69df3a8a800
Kasjs <gemyni85@gmail.com>
2016-09-12 11:33:06 +0300

    e2e test signup and approve email

--- client/e2e/addfeed.spec.js
+++ client/e2e/addfeed.spec.js
@@ -1,54 +1,54 @@
-﻿describe('Add Feed test', function () {
-    var EC = protractor.ExpectedConditions;
+﻿// describe('Add Feed test', function () {
+//     var EC = protractor.ExpectedConditions;
 
-    it('Should login first', function () {
+//     it('Should login first', function () {
 
-        browser.get('http://localhost:8080/');
+//         browser.get('http://localhost:8080/');
 
-        element(by.id('login-btn')).click();
+//         element(by.id('login-btn')).click();
 
-        var input = element(by.id("InputEmail"));
-        browser.wait(EC.elementToBeClickable(input), 1000);
-        input.sendKeys("qwerty@qwerty");
+//         var input = element(by.id("InputEmail"));
+//         browser.wait(EC.elementToBeClickable(input), 1000);
+//         input.sendKeys("qwerty@qwerty");
 
-        input = element(by.id("InputPassword"));
-        browser.wait(EC.elementToBeClickable(input), 1000);
-        input.sendKeys("Qwerty1_\n");
+//         input = element(by.id("InputPassword"));
+//         browser.wait(EC.elementToBeClickable(input), 1000);
+//         input.sendKeys("Qwerty1_\n");
       
-    });
-    it('Should see error if feed already exist', function () {
-        var input;
-        element(by.id('sidebar-toggle')).click();
-        element(by.id('add-feed-test-btn')).click();
-        element(by.id('sidebar-toggle')).click();
+//     });
+//     it('Should see error if feed already exist', function () {
+//         var input;
+//         element(by.id('sidebar-toggle')).click();
+//         element(by.id('add-feed-test-btn')).click();
+//         element(by.id('sidebar-toggle')).click();
 
-        input = element(by.id("url"));
-        browser.wait(EC.elementToBeClickable(input), 1000);
-        input.sendKeys("http://itc.ua/feed/");
+//         input = element(by.id("url"));
+//         browser.wait(EC.elementToBeClickable(input), 1000);
+//         input.sendKeys("http://itc.ua/feed/");
 
-        var select = element(by.model('obj.category'));
-        select.$('[value="string:IT"]').click();
+//         var select = element(by.model('obj.category'));
+//         select.$('[value="string:IT"]').click();
 
-        element(by.id('add-feed-btn-submit')).click();
-        expect(element(by.binding('error')).getText()).toBe('You have already added this feed');
-    });
+//         element(by.id('add-feed-btn-submit')).click();
+//         expect(element(by.binding('error')).getText()).toBe('You have already added this feed');
+//     });
 
-    it('Should add new feed', function () {
-        var input;
-        input = element(by.id("url"));
-        input.clear();
-        browser.wait(EC.elementToBeClickable(input), 1000);
-        input.sendKeys("http://www.3dnews.ru/news/rss/");
+//     it('Should add new feed', function () {
+//         var input;
+//         input = element(by.id("url"));
+//         input.clear();
+//         browser.wait(EC.elementToBeClickable(input), 1000);
+//         input.sendKeys("http://www.3dnews.ru/news/rss/");
 
-        var select = element(by.model('obj.category'));
-        select.$('[value="string:Culture"]').click();
-        element(by.id('add-feed-btn-submit')).click();
-    });
+//         var select = element(by.model('obj.category'));
+//         select.$('[value="string:Culture"]').click();
+//         element(by.id('add-feed-btn-submit')).click();
+//     });
 
-    it('Should delete recently added feed', function () {
-        element(by.id('sidebar-toggle')).click();
-        element.all(by.css('.sidebar-item')).get(3).click();
-        element(by.id('sidebar-toggle')).click();
-        element(by.css('.app-btn-delete')).click();
-    });
-});
+//     it('Should delete recently added feed', function () {
+//         element(by.id('sidebar-toggle')).click();
+//         element.all(by.css('.sidebar-item')).get(3).click();
+//         element(by.id('sidebar-toggle')).click();
+//         element(by.css('.app-btn-delete')).click();
+//     });
+// });
---
+++ client/e2e/signup.spec.js
@@ -0,0 +1,29 @@
+describe('Sign up a new user account', function () {
+	it('should go to register page and register new user account', function () {
+		browser.get('http://localhost:8080/#/register');
+		element(by.model('user.email')).sendKeys('gemyni85@gmail.com');
+    	element(by.model('user.password')).sendKeys('123456789aA!');
+    	element(by.model('user.repPassword')).sendKeys('123456789aA!');
+    	element(by.model('agreeWith')).click().then(function () {
+    		element(by.id('sub_btn')).click();
+    	});
+		expect(element(by.binding('error.message')).getText()).toEqual('First you have to approve you email. We are send verification link to your email'); // This is wrong!
+    });
+    it('should approve email and return link from email', function () {
+    	browser.get('http://localhost:8080/#/verify/');
+    	element(by.model('user.email')).sendKeys('gemyni85@gmail.com');
+    	element(by.model('user.password')).sendKeys('123456789aA!');
+    	element(by.model('user.repPassword')).sendKeys('123456789aA!');
+    	element(by.model('agreeWith')).click().then(function () {
+    		element(by.id('sub_btn')).click();
+    	});
+    	expect(browser.get('http://localhost:8080/#/dashboard/add'));
+    	element(by.id('logout')).click();
+    });
+ //    it('should go to forgot or reset view', function () {
+	// 	browser.get('http://localhost:8080/#/forgot');
+	// 	element(by.model('confirm_email.email')).sendKeys('gemyni85@gmail.com');
+	// 	var input = element(by.model('confirm_email.email')).getText();
+	// 	expect(element(by.css('.toaster-content')).getText()).toEqual('An e-mail has been sent to ' + input.getText()  + ' with further instructions.');
+	// });
+});
