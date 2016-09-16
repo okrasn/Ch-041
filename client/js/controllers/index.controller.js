023c5fbf843a2a08b12d2eaa7fb41ca35b1287a7
nromanen <nromanen.softserveinc@gmail.com>
2016-09-16 17:56:26 +0300

    del mastre files

--- client/js/controllers/index.controller.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    'use strict';
-    angular.module('rssreader').controller('IndexController', ['$scope', '$state', 'authService', '$window', 'themeService', function ($scope, $state, authService, $window, themeService) {
-        $scope.layout = themeService.getTheme;
-        $scope.text = "some text";
-    }]);
-})();
