b745ac67c85072ede1e913038d24b2370c779a73
Valik <valik.stets@gmail.com>
2016-09-27 15:11:32 +0300

    Fixing token sending

--- client/js/services/sessionInjector.js
+++ client/js/services/sessionInjector.js
@@ -1,10 +1,11 @@
 ﻿(function () {
     'use strict';
-    angular.module('rssreader').factory('sessionInjector', ['authService', function (authService) {
+    angular.module('rssreader').factory('sessionInjector', ['$injector', function ($injector) {
         var sessionInjector = {
             request: function (config) {
-                if (authService.isLoggedIn()) {
-                    config.headers['Authorization'] = 'Bearer ' + authService.getToken();
+                var auth = $injector.get('authService');
+                if (auth.isLoggedIn()) {
+                    config.headers['Authorization'] = 'Bearer ' + auth.getToken();
                 }
                 return config;
             }
